48a02696b84272aba067f36c698eca06ef587074
elif-sude-tokis <eliftokis66@gmail.com>
2026-04-12 22:11:25 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float sayi1;
+    float sayi2;
+    float sayi3;
+    float toplam;
+    
+
+
+    cout<<"sayi biri giriniz:";
+    cin>>sayi1;
+
+    cout<<"sayi ikiyi giriniz:";
+    cin>>sayi2;
+
+    cout<<"sayi ücü giriniz:";
+    cin>>sayi3;
+    
+    toplam=sayi1+sayi2+sayi3;
+
+    cout<<"toplam"<<toplam<<endl;
+    return 0;
+}
